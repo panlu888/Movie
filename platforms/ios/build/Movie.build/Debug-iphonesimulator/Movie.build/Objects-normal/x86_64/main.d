bc89cb2ecdef9c51967d9d430cc43fa5ee82a539
luyao <luyao@lextdeMacBook-Pro.local>
2017-03-03 14:09:43 +0800

    project setup

---
+++ platforms/ios/build/Movie.build/Debug-iphonesimulator/Movie.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/main.m \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/luyao/NativeScript/Movie/platforms/ios/internal/TNSDebugging.h
