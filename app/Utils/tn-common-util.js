f98dca0e91212c7a971f5451b8a95173484abb6b
panzhou.zpp <panzhou.zpp@alibabapictures.com>
2017-03-07 21:25:30 +0800

    tab color

---
+++ app/Utils/tn-common-util.js
@@ -0,0 +1,35 @@
+var colorModule = require("color");
+//TODO: exports common color
+var whiteColor = new colorModule.Color('ffffff');
+var blackColor = new colorModule.Color('000000');
+var yellowColor = new colorModule.Color("F4B400");
+exports.whiteColor = whiteColor;
+exports.blackColor = blackColor;
+exports.yellowColor = yellowColor;
+
+exports.color_dark1 = new colorModule.Color("4D4F53");
+exports.color_dark2 = new colorModule.Color("999CA1");
+exports.color_dark3 = new colorModule.Color("A9ACB1");
+exports.color_dark4 = new colorModule.Color("E2E2E2");
+
+exports.color_light1 = new colorModule.Color("cccccc");
+exports.color_light2 = new colorModule.Color("E5E5E5");
+
+exports.color_red = new colorModule.Color("F44336");
+exports.color_red2 = new colorModule.Color("DE4330");
+
+exports.color_text_grey = new colorModule.Color("999ca1");
+exports.color_text_disable = new colorModule.Color("D4D6D8");
+
+exports.color_orange = new colorModule.Color("FEA54C");
+exports.color_blue = new colorModule.Color("2196F3");
+exports.color_green = new colorModule.Color("4CAF50");
+exports.color_lightblue = new colorModule.Color("eef9ff");
+
+exports.color_pink = new colorModule.Color("fc4d6d");
+exports.color_purple = new colorModule.Color("673AB7");
+exports.color_separator = new colorModule.Color("E2E2E2");
+exports.color_tabground = new colorModule.Color(100,36,41,49);
+
+exports.color_background = new colorModule.Color("F5F5F5");
+exports.navigationBarDefaultBackgroudColor = new colorModule.Color("FFFFFF");
--- app/main-page.js
+++ app/main-page.js
@@ -1,8 +1,19 @@
 var createViewModel = require("./main-view-model").createViewModel;
+var color = require("./Utils/tn-common-util.js");
 
 function onNavigatingTo(args) {
     var page = args.object;
     page.bindingContext = createViewModel();
 }
-
+exports.pageLoaded = function(args) {
+  page = args.object;
+  var tabView = page.getViewById("tabView");
+  tabView.selectedColor = color.yellowColor;
+  
+  tabView.tabsBackgroundColor = color.color_dark4;
+  tabView.tabTextColor = color.whiteColor;
+  tabView.tabBackgroundColor = color.color_tabground;
+  tabView.selectedColor = color.yellowColor;
+  tabView.selectedColor = color.yellowColor;
+};
 exports.onNavigatingTo = onNavigatingTo;
