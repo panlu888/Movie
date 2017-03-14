8e4dbadef151535bae198ec5cd3e85190daca224
luyao <luyao@lextdeMacBook-Pro.local>
2017-03-15 00:23:51 +0800

    some style changes.

--- app/main-page.js
+++ app/main-page.js
@@ -1,8 +1,10 @@
 var color = require("./Utils/tn-common-util.js"),
   apiCall = require('./Utils/tn-api-call.js').apiCall,
   observable = require("data/observable"),
+  utilityModule = require("utils/utils"),
   _ = require('lodash');
-var screen_width_inDP = require("platform").screen.mainScreen.widthPixels/require("platform").screen.mainScreen.scale;
+
+var screen_width_inDP = require("platform").screen.mainScreen.widthPixels / require("platform").screen.mainScreen.scale;
 
 // Create view model to expose data to view
 var viewModel = new observable.Observable();
@@ -13,32 +15,37 @@ function setTabView(tabView) {
   tabView.tabTextColor = color.whiteColor;
   tabView.tabBackgroundColor = color.color_tabground;
 }
-exports.selectedIndexChanged = function (args){
+
+exports.selectedIndexChanged = function (args) {
   page = args.object;
-  if(page != undefined){
+  if (page != undefined) {
     var tabView = page.getViewById("tabView");
     var index = tabView.selectedIndex;
     var item = tabView.items[index];
-    viewModel.set("naviTitle",item.title);
-    if(index == 1 && (viewModel.theatres == undefined || viewModel.theatres.length <= 0)){
-    apiCall.getData('shows').then(function (response) {
-    viewModel.set("theatres", response.results);
-  });
+    viewModel.set("naviTitle", item.title);
+    if (index == 1 && (viewModel.theatres == undefined || viewModel.theatres.length <= 0)) {
+      apiCall.getData('shows').then(function (response) {
+        viewModel.set("theatres", response.results);
+      });
     }
   }
 };
+
+exports.gridViewItemTap = function (args) {
+  console.log('hello world');
+};
+
 
 exports.pageLoaded = function (args) {
   page = args.object;
   var tabView = page.getViewById("tabView");
   setTabView(tabView);
-  viewModel.set("naviTitle","Movies");
-  viewModel.set("screenWidth",screen_width_inDP);
-  viewModel.set("movies",[]);
-  viewModel.set("theatres",[]);
+  viewModel.set("naviTitle", "Movies");
+  viewModel.set("screenWidth", screen_width_inDP);
+  viewModel.set("movies", []);
+  viewModel.set("theatres", []);
   page.bindingContext = viewModel;
   apiCall.getData('movies').then(function (response) {
     viewModel.set("movies", response.results);
-
   });
 };
