1798bc291b86ed1aa158af7abf2216e1d9e190af
luyao <luyao@lextdeMacBook-Pro.local>
2017-03-24 16:46:58 +0800

    include the css files for register and login page.

--- app/Utils/el-user-manager.js
+++ app/Utils/el-user-manager.js
@@ -4,36 +4,36 @@ var userData = {
     el_app_key: function (id) {
         return 'esdcmmd4xv91ije0';
     },
-    tn_login:function(name,password){
+    tn_login: function (name, password) {
         var everlive = new Everlive({
-        appId: 'esdcmmd4xv91ije0',
-        scheme: "https"
+            appId: 'esdcmmd4xv91ije0',
+            scheme: "https"
         });
-        return everlive.authentication.login(name, password, function(data) {
-        return data;
-        
-    }, function(err) {
+        return everlive.authentication.login(name, password, function (data) {
+            return data;
+
+        }, function (err) {
             return err;
         });
     },
-    tn_register:function(name,pwd){
+    tn_register: function (name, pwd) {
         var everlive = new Everlive({
-        appId: 'esdcmmd4xv91ije0',
-        scheme: "https"
+            appId: 'esdcmmd4xv91ije0',
+            scheme: "https"
         });
         var attrs = {
             Email: name,
             //DisplayName: name
         };
         everlive.Users.register(name,
-        pwd,
-        attrs,
-        function(data) {
-            alert(JSON.stringify("sussce"+data));
-        },
-        function(error) {
-            alert(JSON.stringify("======"+error));
-        });
-    },
+            pwd,
+            attrs,
+            function (data) {
+                alert(JSON.stringify("sussce" + data));
+            },
+            function (error) {
+                alert(JSON.stringify("======" + error));
+            });
+    }
 };
 exports.userData = userData;
